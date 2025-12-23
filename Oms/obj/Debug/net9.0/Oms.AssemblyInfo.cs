a29d82b7cfcca7aee15b9529afc3079b8e48657a
LittleBlBb <apdragon2014@gmail.com>
2025-12-23 17:30:24 +0300

    aaaaaaaaaaaa

--- Oms/obj/Debug/net9.0/Oms.AssemblyInfo.cs
+++ Oms/obj/Debug/net9.0/Oms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d658822c3a6a966240cc35fcabdc432bda29be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7af008580553d912937895254362445a466897d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce08f3803098f3ad58572691f8a2d6666a817ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7af008580553d912937895254362445a466897d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
